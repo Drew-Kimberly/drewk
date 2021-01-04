7e9408094b617eb34503b0f4643e6d2c00ff510d
Drew Kimberly <drew.kimberly@citrix.com>
2021-01-03 20:16:58 -0500

    feat(react-ui/navbar): bootstrap react-ui-navbar lib

---
+++ libs/react-ui/navbar/src/lib/navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/* eslint-disable-next-line */
+export interface NavbarProps {}
+
+export function Navbar(props: NavbarProps) {
+  return (
+    <div>
+      <h1>Welcome to react-ui-navbar!</h1>
+    </div>
+  );
+}
+
+export default Navbar;
